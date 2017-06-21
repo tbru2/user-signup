084f41bba093aa5cd210a9ce53cbfac5daaf2765
tbru2 <tbru2@pdx.edu>
2017-06-21 14:37:48 -0700

    finished validation

---
+++ main.py
@@ -0,0 +1,67 @@
+from flask import Flask, request, redirect, render_template
+import cgi
+import os
+
+app = Flask(__name__)
+app.config['DEBUG']=True
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/", methods=['POST'])
+def signup():
+    username = request.form['username']
+    password = request.form['password']
+    verify_password = request.form['verify_password']
+    email = request.form['email']
+    
+    error_check = False
+    username_error = ''
+    password_error = ''
+    verify_password_error = ''
+    email_error = ''
+
+    if " " in username:
+        username_error = "Space(s) not allowed."
+        error_check = True
+    if len(username) < 3 or len(username > 20):
+        username_error = "Username is not the proper length."
+        error_check = True
+    if password != verify_password:
+        verify_password_error = "Passwords don't match."
+        password = ''
+        verify_password = ''
+        error_check = True
+    if ' ' in password:
+        password_error = "Space(s) not allowed."
+        password = ''
+        verify_password = ''
+        error_check = True
+    if len(password) < 3 or len(password) > 20:
+        password_error = "Password is not the proper length."
+        password = ''
+        verify_password = ''
+        error_check = True
+    if email != '':
+        if email.count('@') != 1:
+            email_error = "Not a valid email address."
+            error_check = True
+        if email.count('.') != 1:
+            email_error = "Not a valid email address."
+            error_check = True
+        if " " in email:
+            email_error = "Not a valid email address."
+            error_check = True
+        if len(email) < 3 or len(email) > 20:
+            email_error = "Not a valid email address."
+            error_check = True
+    if error_check == True:
+        return render_template("index.html", username_error=username_error,
+            password_error=password_error, verify_password_error=verify_password_error,
+            email_error=email_error, username=username, password=password,
+            verify_password=verify_password, email=email)
+    else:
+        return render_template("welcome.html", username = username)
+
+app.run()
